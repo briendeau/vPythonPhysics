427110cea4fa26f1e25558e6264b2c19fdd63be4
briendeau <yamori425@gmail.com>
2022-05-19 23:33:14 -0400

    playing with tuples

---
+++ tuples1.py
@@ -0,0 +1,21 @@
+name = input("Enter file: ")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+di =  dict()
+    
+for line in handle:
+    if line.startswith("From "):
+        words = line.rstrip().split()
+        for word in words:
+            time = words[5].split(':')
+            extr = time[0]
+            di[extr] = di.get(extr, 0) + 1
+
+li = list()
+for k, v in di.items():
+    tup = (k, v)
+    li.append(tup)
+    
+for k, v in sorted(li):
+    print(k, v)
